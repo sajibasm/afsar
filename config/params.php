1602e48c82d46b75f44cf380e8c01abb9126b1d6
ASMSAJIB <sajib.cse03@gmail.com>
2024-05-25 17:40:33 -0400

    updated configuration v3

--- config/params.php
+++ config/params.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'supportEmail' => 'axialsolutionltd@gmail.com',
-    'adminEmail' => 'admin@asmsajib.me',
+    'supportEmail' => getenv('PARAMS_SUPPORT_EMAIL'),
+    'adminEmail' => getenv('PARAMS_ADMIN_EMAIL'),
     'timeZone'=>'Asia/Dhaka',
     'currency'=>'BDT',
     'pageSize'=>50,
-    'secretKey'=>'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+    'secretKey'=>getenv('PARAMS_SECRET_KEY'),
     'calendarFormat'=>'dd/m/yyyy',
     'gridviewHeaderColor'=>\kartik\grid\GridView::TYPE_INFO,
 ];
